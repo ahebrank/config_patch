87c476961f7a13e182a4a33b81eb6cb449f03b91
a.dmitriiev <a.dmitriiev@3235287.no-reply.drupal.org>
2020-01-03 08:19:52 -0500

    Issue #3102448 by a.dmitriiev: Create drush/drupal console command to output patch

--- src/Commands/ConfigPatchCommands.php
+++ src/Commands/ConfigPatchCommands.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\config_patch\Commands;
 
+use Drupal\Component\Render\MarkupInterface;
 use Drupal\config_patch\ConfigCompare;
 use Drupal\config_patch\Plugin\config_patch\output\CliOutputPluginInterface;
 use Drush\Commands\DrushCommands;
@@ -30,11 +31,11 @@ class ConfigPatchCommands extends DrushCommands {
   /**
    * Create a configuration patch.
    *
-   * @command config-patch-output
+   * @command config:patch
    * @param $plugin_id Id of the output plugin.
    * @option filename Filename where to output the patch.
    * @option collections A comma separated list of included collections.
-   * @aliases cpo
+   * @aliases cpatch
    */
   public function outputPatch($plugin_id, $options = ['filename' => '', 'collections' => '']) {
     $this->configCompare->setOutputPlugin($plugin_id);
@@ -50,12 +51,15 @@ class ConfigPatchCommands extends DrushCommands {
     }
     if ($this->configCompare->getOutputPlugin() instanceof CliOutputPluginInterface) {
       $output = $this->configCompare->getOutputPlugin()
-        ->outputCli($patches, $change_list);
+        ->outputCli($patches, $change_list, $options);
       if (!empty($options['filename'])) {
         file_put_contents($options['filename'], $output);
       }
       else {
-        $this->output()->write($output);
+        if ($output instanceof MarkupInterface) {
+          $output = $output->__toString();
+        }
+        $this->output()->writeln($output);
       }
     }
     else {
